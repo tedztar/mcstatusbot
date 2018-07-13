33b9d0288f2bc77f107e3f493567f742df1d1328
lerokko <34169936+lerokko@users.noreply.github.com>
2018-07-13 21:40:21 +0200

    Add files via upload

---
+++ mcbot.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var statustring = "No signal";
+
+var request = require('request');
+var mcCommand = '/minecraft'; // Command for triggering
+var mcIP = 'play.server26.net'; // Your MC server IP
+var mcPort = 25565; // Your MC server port
+
+var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+
+
+function update() {
+  /*seconds = seconds + 1;
+  secondsString = seconds.toString();
+  client.user.setActivity(secondsString, { type: 'Playing' })
+  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+  .catch(console.error);*/
+  request(url, function(err, response, body) {
+      if(err) {
+          console.log(err);
+          //return message.reply('Error getting Minecraft server status...');
+      }
+      body = JSON.parse(body);
+      var status = 'Server offline';
+      console.log(body.motd);
+      if(body.online) {
+          if(body.motd=="§c§oWe are under maintenance."){
+            client.user.setStatus('idle')
+            //.then(console.log)
+            .catch(console.error);
+          }else{
+            client.user.setStatus('online')
+            //.then(console.log)
+            .catch(console.error);
+          }
+            if(body.players.now) {
+                status = ' ' + body.players.now + '  of  ' + body.players.max;
+              } else {
+                status = ' 0  of  ' + body.players.max;
+        }
+      } else {
+        client.user.setStatus('dnd')
+        //.then(console.log)
+        .catch(console.error);
+
+      }
+      client.user.setActivity(status, { type: 'PLAYING' })
+      .then(presence => console.log(status))
+      .catch(console.error);
+  });
+
+}
+client.on("ready", () => {
+  console.log("I am ready!");
+  client.setInterval(update,30000);
+});
+
+/*client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+    update();
+
+  }
+}
+);*/
+
+client.login("NDY3MzQxODQ2ODEzNjA1ODg4.DipPfQ.zIUELooLYfTwmz4agqyIy8-b0cU");
