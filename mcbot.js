bd04257b67d35de3d949255c81609c3766295d62
lerokko <34169936+lerokko@users.noreply.github.com>
2018-07-13 21:43:06 +0200

    Update mcbot.js

--- mcbot.js
+++ mcbot.js
@@ -1,3 +1,4 @@
+//credit to vegeta897 for the request URL part from his 'Simple Minecraft server status bot'
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var statustring = "No signal";
