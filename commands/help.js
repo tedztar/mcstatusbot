e55d30e145dc5323ae33c74c985f640799758049
tedztar <tedztar@gmail.com>
2020-03-05 17:06:23 +1300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,6 @@
-exports.run = async (client, message, args) => {
+const Discord = require("discord.js");
+
+exports.run = async (settings, client, message, args) => {
 	//code to run when command is sent
 	console.log('Issuing help message.');
              const helpEmbed = new Discord.RichEmbed()
