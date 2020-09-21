71bf4a5e4c27dd6215321207ff13d558b3481440
tedztar <tedztar@gmail.com>
2020-09-21 17:21:15 +1200

    Fixed dependancy issue
    Issue #32

--- commands/status.js
+++ commands/status.js
@@ -1,5 +1,6 @@
 const mcping = require('mc-ping-updated');
 const Discord = require("discord.js");
+const escape = require('markdown-escape');
 
 exports.run = async (settings, client, message, args) => {
 	//code to run when command is sent
