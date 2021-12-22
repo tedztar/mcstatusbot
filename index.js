7ab8e3c73c5d641e080230c52ec6d1eaa08fc9ef
thedjchi <achiommino@gmail.com>
2021-12-21 23:23:30 -0500

    trying to fix heroku

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ serverDB.on('error', err => console.error('Keyv connection error:', err));
 
 // Command Handler
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
 // Event Handler
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
+	const event = require(`events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args, client));
 	}
