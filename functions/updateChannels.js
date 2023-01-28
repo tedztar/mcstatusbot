a081dd18a3b00bcb6c74cf9bf0db68426b56fc58
RahulR100 <rahulrao@email.com>
2023-01-28 09:38:00 -0800

    update status checker

--- functions/updateChannels.js
+++ functions/updateChannels.js
@@ -3,15 +3,15 @@ const mcping = require('mcping-js');
 module.exports = {
 	async execute(server) {
 		const mcserver = new mcping.MinecraftServer(server.ip.split(':')[0], Number(server.ip.split(':')[1]));
-		const statusChannel = await client.channels.cache.get(server.statusId);
-		const playersChannel = await client.channels.cache.get(server.playersId);
 
 		try {
+			const statusChannel = await client.channels.cache.get(server.statusId);
+			const playersChannel = await client.channels.cache.get(server.playersId);
+
 			mcserver.ping(2500, 47, async (err, res) => {
 				err ? await setOffline(statusChannel, playersChannel) : await setOnline(statusChannel, playersChannel, res);
 			});
 		} catch (error) {
-			await setOffline(statusChannel, playersChannel);
 			console.log(`${error.code}: encountered while updating ${statusChannel}, ${playersChannel}`);
 		}
 	}
